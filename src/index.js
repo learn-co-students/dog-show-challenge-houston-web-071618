09baed08f7e2fd0a194e87cede073ffea7a37718
Christopher Arnold <chrisarnold2010@gmail.com>
2018-09-06 16:56:16 -0500

    fully functional

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	const dogsURL = 'http://localhost:3000/dogs'
 	const parsedJSON = response => response.json()
 	const registeredDogsTable = document.querySelector('#table-body')
+	const divForm = document.querySelector('#dog-form')
 
 	fetch(dogsURL)
 		.then(parsedJSON)
@@ -10,23 +11,44 @@ document.addEventListener('DOMContentLoaded', () => {
 			dogs.forEach(dog => {
 				registeredDogsTable.innerHTML += `
 					<tr>
-						<td class='padding center'>${dog.name}</td> 
-						<td class='padding center'>${dog.breed}</td> 
-						<td class='padding center'>${dog.sex}</td> 
-						<td class='padding center'><button id="${dog.id}">Edit</button></td>
+						<td class='padding center' id="name-${dog.id}">${dog.name}</td> 
+						<td class='padding center' id="breed-${dog.id}">${dog.breed}</td> 
+						<td class='padding center' id="sex-${dog.id}">${dog.sex}</td> 
+						<td class='padding center'><button data-id="${dog.id}">Edit</button></td>
 					</tr>
 				`
 			})
 		})
 
+	registeredDogsTable.addEventListener('click', addDogToFormForEdit)
+	
 
-	registeredDogsTable.addEventListener('click', editDog)
+	function addDogToFormForEdit(event) {
+
+		event.preventDefault();
+
+		divForm[0].value = document.getElementById(`name-${event.target.dataset.id}`).innerHTML
+		divForm[1].value = document.getElementById(`breed-${event.target.dataset.id}`).innerHTML
+		divForm[2].value = document.getElementById(`sex-${event.target.dataset.id}`).innerHTML
+		divForm.dataset.id = event.target.dataset.id
+
+	}
+
+	divForm.addEventListener('submit', editDog)
 
 	function editDog(event) {
-		event.preventDefault();
+		// event.preventDefault();
 		
-		fetch(dogsURL + '/' + `${dog.id}`, {
-
+		fetch(dogsURL + '/' + event.target.dataset.id, {
+			method: 'PATCH',
+			body: JSON.stringify({
+				    "name": divForm[0].value,
+				    "breed": divForm[1].value,
+				    "sex": divForm[2].value
+			}),
+			headers: {
+				"Content-Type" : "application/json"
+			}
 		})
 	}
 
