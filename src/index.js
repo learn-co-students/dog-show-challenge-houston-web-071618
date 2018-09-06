4da20fa5629a3a3018cbbe6101d90b6bea8bebc4
Christopher Arnold <chrisarnold2010@gmail.com>
2018-09-06 14:47:44 -0500

    full list render complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+	const dogsURL = 'http://localhost:3000/dogs'
+	const parsedJSON = response => response.json()
+	const registeredDogsTable = document.querySelector('#table-body')
+
+	fetch(dogsURL)
+		.then(parsedJSON)
+		.then(function showDogs(dogs) {
+			dogs.forEach(dog => {
+				registeredDogsTable.innerHTML += `
+					<tr>
+						<td class='padding center'>${dog.name}</td> 
+						<td class='padding center'>${dog.breed}</td> 
+						<td class='padding center'>${dog.sex}</td> 
+						<td class='padding center'><button id="${dog.id}">Edit</button></td>
+					</tr>
+				`
+			})
+		})
+
+
+	registeredDogsTable.addEventListener('click', editDog)
+
+	function editDog(event) {
+		event.preventDefault();
+		
+		fetch(dogsURL + '/' + `${dog.id}`, {
+
+		})
+	}
+
+
 })
