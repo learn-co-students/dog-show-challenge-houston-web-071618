843bbeb8cc9935ab6188caade782be24ca32fcc2
e-barr <github email address>
2018-09-06 14:49:16 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,67 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const tableDiv = document.querySelector('table')
+  const editDogForm = document.getElementById('dog-form')
+  const editInputs = editDogForm.querySelectorAll('input')
+  const editName = editInputs[0]
+  const editBreed = editInputs[1]
+  const editSex = editInputs[2]
+
+  tableDiv.addEventListener('click', editDog)
+  editDogForm.addEventListener('submit', updateDog)
+
+  function fetchDogs() {
+    fetch('http://localhost:3000/dogs')
+      .then(resp => resp.json())
+      .then(resp => renderDogs(resp))
+  }
+
+  function renderDogs(dogs) {
+    dogs.forEach(dog => renderOneDog(dog))
+  }
+
+  function renderOneDog(dog) {
+    let newDogTemplate = `<tr id=${dog.id}><td>${dog.name}</td><td>${dog.breed}</td><td>${dog.sex}</td><td><button class="edit-dog">Edit</button></td></tr>`
+    tableDiv.innerHTML += newDogTemplate
+  }
+
+  function editDog(event) {
+    if (event.target.className === "edit-dog") {
+      let id = event.target.parentElement.parentElement.id
+      let info = event.target.parentElement.parentElement.querySelectorAll('td')
+      let name = info[0].innerText
+      let breed = info[1].innerText
+      let sex = info[2].innerText
+
+      fillEditDogForm(id, name, breed, sex)
+
+    }
+  }
+
+  function fillEditDogForm(id, name, breed, sex) {
+    editName.value = name
+    editName.innerText = name
+    editName.id = id
+    editBreed.value = breed
+    editBreed.innerText = breed
+    editSex.value = sex
+    editSex.innerText = sex
+  }
+
+  function updateDog() {
+    fetch(`http://localhost:3000/dogs/${editName.id}`, {
+      method: "PATCH",
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        id: editName.id,
+        name: editName.value,
+        breed: editBreed.value,
+        sex: editSex.value
+      })
+    })
+  }
+
+  fetchDogs()
 
 })
