dcbd78d93138cf9e408eec588e94e2d9b662d570
Scott Tang <fq600@yahoo.com>
2018-09-07 09:29:20 -0500

    done

--- src/dog.js
+++ src/dog.js
@@ -1,3 +1,7 @@
 class Dog {
-  
+  constructor(name, breed, sex) {
+    this.name = name;
+    this.breed = breed;
+    this.sex = sex;
+  }
 }
--- src/dogController.js
+++ src/dogController.js
@@ -1,3 +1,22 @@
 class DogController {
-  
+  constructor(dogs) {
+    this.dogs = dogs;
+  }
+
+  editDog() {}
+
+  render() {
+    this.dogs.then(json =>
+      json.forEach(dog => {
+        `<tr>
+          <td>${dog.name}</td>
+          <td>${dog.breed}</td>
+          <td>${dog.sex}</td>
+          <td>
+            <button>Edit</button>
+          </td>
+      </tr>`;
+      })
+    );
+  }
 }
